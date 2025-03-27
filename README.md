a6a63d8c0bc3810881db27aecbce357587fc2240
Rosa-Herrero <rherrero@tecnocampus.cat>
2025-03-27 12:44:55 +0100

    Initial

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # Practica1Students
- 
+
+Indica el teu nom:
+
+Indica el teu usuari:
+
+Indica la teva parella: 
---
+++ test/CapaDomini/MultiplesTest.java
@@ -0,0 +1,19 @@
+package CapaDomini;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class MultiplesTest {
+
+    @Test
+    public void test1()
+    { fail();}
+
+    @Test
+    public void test2()
+    { assertTrue(true);}
+
+    @Test
+    public void test3() throws Exception { throw new Exception();}
+}
